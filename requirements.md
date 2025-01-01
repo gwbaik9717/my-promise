10572f27f6d072421e42e48f8e969efc0cef2a4f
Paik <gwbaik9717@icloud.com>
2025-01-01 16:16:36 +0900

    feat: then 메서드는 두 개의 콜백 함수를 인자로 받는데, 첫 번째 콜백 함수는 onfullfilledQueue에 저장되고, 두 번째 콜백 함수는 onrejectedQueue에 저장된다. catch 메서드는 한 개의 콜백 함수를 인자로 받는데, onrejectedQueue 에 저장된다.

--- requirements.md
+++ requirements.md
@@ -8,6 +8,6 @@
 - [x] resolve 함수를 호출하면 fulfilled 상태로 변경된다.
 - [x] reject 함수를 호출하면 rejected 상태로 변경된다.
 - [x] resolve, reject 함수는 인자로 아무 값이나 받을 수 있다.
-- [ ] Promise의 상태가 fulfilled 이면 then 메소드의 콜백 함수가 실행된다.
-- [ ] Promise의 상태가 rejected 이면 catch 메소드의 콜백 함수가 실행된다.
+- [x] then 메서드는 두 개의 콜백 함수를 인자로 받는데, 첫 번째 콜백 함수는 onfullfilledQueue에 저장되고, 두 번째 콜백 함수는 onrejectedQueue에 저장된다.
+- [x] catch 메서드는 한 개의 콜백 함수를 인자로 받는데, onrejectedQueue에 저장된다.
 - [ ] then 메소드는 새로운 Promise를 반환한다.
--- src/promise.ts
+++ src/promise.ts
@@ -6,9 +6,14 @@ type Executor<T> = (
 ) => void;
 export class MyPromise<T> {
   private status: MyPromiseStatus;
+  private onfulfilledQueue: ((value: T) => void)[];
+  private onrejectedQueue: ((reason: any) => void)[];
 
   constructor(executor: Executor<T>) {
     this.status = "pending";
+
+    this.onfulfilledQueue = [];
+    this.onrejectedQueue = [];
 
     executor(this.resolve.bind(this), this.reject.bind(this));
   }
@@ -20,4 +25,19 @@ export class MyPromise<T> {
   private reject(reason: any) {
     this.status = "rejected";
   }
+
+  then(onfulfilled: (value: T) => void, onrejected: (reason: any) => void) {
+    this.onfulfilledQueue.push(onfulfilled);
+    this.onrejectedQueue.push(onrejected);
+  }
+
+  catch(onrejected: (reason: any) => void) {
+    this.onrejectedQueue.push(onrejected);
+  }
 }
+
+const promise = new Promise<string>((res, rej) => {
+  res("abc");
+});
+
+promise.then().catch();
